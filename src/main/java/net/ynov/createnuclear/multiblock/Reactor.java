d03958baba7f79ccbc229905622fb14ca33b456d
bdimitri <dimitri.brancourt@ynov.com>
2024-01-10 15:08:36 +0100

    being bullied by the multiblock

--- src/main/java/net/ynov/createnuclear/multiblock/Reactor.java
+++
@@ -1,4 +0,0 @@
-package net.ynov.createnuclear.multiblock;
-
-public class Reactor {
-}
---
+++ src/main/java/net/ynov/createnuclear/multiblock/ReactorBlock.java
@@ -0,0 +1,10 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
+package net.ynov.createnuclear.multiblock;
+
+public class ReactorBlock {
+
+}
