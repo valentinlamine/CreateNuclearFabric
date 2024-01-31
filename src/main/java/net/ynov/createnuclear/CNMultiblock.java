d37e196cc2e41925e77b0314b3a2c729beabe108
thomasdubuis <thomasdubuis13@gmail.com>
2024-01-31 14:43:16 +0100

    Create real multiblock

--- src/main/java/net/ynov/createnuclear/CNMultiblock.java
+++ src/main/java/net/ynov/createnuclear/CNMultiblock.java
@@ -1,9 +1,8 @@
 package net.ynov.createnuclear;
 
 import lib.multiblock.test.SimpleMultiBlockAislePatternBuilder;
-import lib.multiblock.test.SimpleMultiBlockPattern;
+import net.minecraft.world.level.block.Blocks;
 import net.ynov.createnuclear.block.CNBlocks;
-import net.ynov.createnuclear.fluid.CNFluids;
 import net.ynov.createnuclear.multiblock.Exemple;
 import net.ynov.createnuclear.multiblock.MultiBlockManagerBeta;
 
@@ -15,8 +14,17 @@ public class CNMultiblock {
         REGISTRATE_MULTIBLOCK.register("createnuclear:test",
                 Exemple.EXEMPLE,
                 SimpleMultiBlockAislePatternBuilder.start()
-                    .aisle("LLL", "L*L", "LLL")
-                    .where('L', a -> a.getState().is(CNBlocks.DEEPSLATE_URANIUM_ORE.get()))
+                    .aisle("AAAAA", "AAAAA", "AAAAA", "AAAAA", "AAAAA")
+                    .aisle("AABAA", "ADADA", "BACAB", "ADADA", "AABAA")
+                    .aisle("AABAA", "ADADA", "BACAB", "ADADA", "AABAA")
+                        .aisle("AABAA", "ADADA", "BACAB", "ADADA", "AA*AA")
+                    .aisle("AABAA", "ADADA", "BACAB", "ADADA", "AABAA")
+                    .aisle("AABAA", "ADADA", "BACAB", "ADADA", "AABAA")
+                    .aisle("AAAAA", "AAAAA", "AAAAA", "AAAAA", "AAAAA")
+                    .where('A', a -> a.getState().is(Blocks.DIAMOND_BLOCK))
+                    .where('B', a -> a.getState().is(Blocks.GOLD_BLOCK))
+                    .where('C', a -> a.getState().is(Blocks.NETHERITE_BLOCK))
+                    .where('D', a -> a.getState().is(Blocks.AMETHYST_BLOCK))
                     .where('*', a -> a.getState().is(CNBlocks.REINFORCED_GLASS.get()))
                 .build()
         );
