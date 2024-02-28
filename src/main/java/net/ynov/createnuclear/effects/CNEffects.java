0fe6f63d41cda7ba4eb843b2babab65b080a5f6e
MathisGredtYnov <113118860+MathisGredtYnov@users.noreply.github.com>
2024-02-28 13:42:18 +0100

    Potion et Arrow fix

--- src/main/java/net/ynov/createnuclear/effects/CNEffects.java
+++ src/main/java/net/ynov/createnuclear/effects/CNEffects.java
@@ -1,12 +1,6 @@
 package net.ynov.createnuclear.effects;
 
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.damagesource.DamageSources;
 import net.minecraft.world.effect.MobEffect;
-import net.minecraft.world.effect.MobEffects;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
-import net.minecraft.world.item.alchemy.Potion;
 import net.ynov.createnuclear.CreateNuclear;
 import io.github.fabricators_of_create.porting_lib.util.LazyRegistrar;
 import io.github.fabricators_of_create.porting_lib.util.RegistryObject;
--- src/main/java/net/ynov/createnuclear/item/CNItems.java
+++ src/main/java/net/ynov/createnuclear/item/CNItems.java
@@ -1,6 +1,8 @@
 package net.ynov.createnuclear.item;
 
 import com.tterrag.registrate.util.entry.ItemEntry;
+
+import io.github.fabricators_of_create.porting_lib.brewing.BrewingRecipeRegistry;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -10,9 +12,15 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.ArrowItem;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.alchemy.Potion;
+import net.minecraft.world.item.alchemy.PotionBrewing;
+import net.minecraft.world.item.alchemy.Potions;
 import net.ynov.createnuclear.CreateNuclear;
+import net.ynov.createnuclear.effects.CNEffects;
 import net.ynov.createnuclear.groups.CNGroup;
 
 public class CNItems {
@@ -24,13 +32,15 @@ public class CNItems {
     public static final Item GRAPHENE = registerItem("graphene", new Item(new FabricItemSettings()));
     public static final Item STEEL_INGOT = registerItem("steel_ingot", new Item(new FabricItemSettings()));
     public static final Item YELLOW_CAKE_NETHER_STAR = registerItem("yellow_cake_nether_star_wip", new Item(new FabricItemSettings()));
-//    public static final Item URANIUM_ROD = registerItem("uranium_rod", new Item(new FabricItemSettings()));
     public static final ItemEntry<Item> URANIUM_ROD = CreateNuclear.REGISTRATE.item("uranium_rod", Item::new).register();
-//    public static final Item GRAPHITE_ROD = registerItem("graphite_rod", new Item(new FabricItemSettings()));
     public static final ItemEntry<Item> GRAPHITE_ROD = CreateNuclear.REGISTRATE.item("graphite_rod", Item::new).register();
     public static final Item RAW_URANIUM = registerItem("raw_uranium", new Item(new FabricItemSettings()));
     public static final Item WELDING_KIT = registerItem("welding_kit", new Item(new FabricItemSettings()));
     public static final Item RAW_LEAD = registerItem("raw_lead", new Item(new FabricItemSettings()));
+    public static final Potion potion_1 = registerPotion("potion_of_radiation_1", new Potion(new MobEffectInstance(CNEffects.RADIATION.get(), 900)));
+    public static final Potion potion_augment_1 = registerPotion("potion_of_radiation_augment_1", new Potion(new MobEffectInstance(CNEffects.RADIATION.get(), 1800)));
+    public static final Potion potion_2 = registerPotion("potion_of_radiation_2", new Potion(new MobEffectInstance(CNEffects.RADIATION.get(), 410, 1)));
+
 
     private static void addItemToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.accept(URANIUM_POWDER, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
@@ -55,6 +65,16 @@ public class CNItems {
     public static void registerCNItems() {
         CreateNuclear.LOGGER.info("Registering mod items for " + CreateNuclear.MOD_ID);
         ItemGroupEvents.modifyEntriesEvent(CNGroup.MAIN_KEY).register(CNItems::addItemToIngredientItemGroup);
+        registerPotionsRecipes();
     }
 
+    public static Potion registerPotion(String name, Potion potion) {
+        return Registry.register(BuiltInRegistries.POTION, new ResourceLocation(CreateNuclear.MOD_ID, name), potion);
+    }
+
+    public static void registerPotionsRecipes() {
+        PotionBrewing.addMix(Potions.AWKWARD, CNItems.YELLOW_CAKE_ENRICHED, CNItems.potion_1);
+        PotionBrewing.addMix(potion_1, Items.REDSTONE, CNItems.potion_augment_1);
+        PotionBrewing.addMix(potion_1, Items.GLOWSTONE_DUST, CNItems.potion_2);
+    }
 }
