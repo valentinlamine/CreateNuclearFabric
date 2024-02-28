2a897241619ce5cd24deb3880348e466c0a54c45
Giovanni <giovanni.ricotta@ynov.com>
2024-02-28 09:03:02 +0100

    craft fan jei

--- src/main/java/net/ynov/createnuclear/compact/jei/category/FanEnrichedCategory.java
+++ src/main/java/net/ynov/createnuclear/compact/jei/category/FanEnrichedCategory.java
@@ -1,12 +1,12 @@
 package net.ynov.createnuclear.compact.jei.category;
 
-import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
 import com.simibubi.create.compat.jei.category.ProcessingViaFanCategory;
 import com.simibubi.create.compat.jei.category.animations.AnimatedKinetics;
-import com.simibubi.create.content.kinetics.fan.processing.HauntingRecipe;
 import com.simibubi.create.foundation.gui.AllGuiTextures;
 import com.simibubi.create.foundation.gui.element.GuiGameElement;
 import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.network.chat.Component;
+import net.ynov.createnuclear.CreateNuclear;
 import net.ynov.createnuclear.block.CNBlocks;
 import net.ynov.createnuclear.fan.EnrichedRecipe;
 
@@ -24,10 +24,15 @@ public class FanEnrichedCategory extends ProcessingViaFanCategory.MultiOutput<En
 
     @Override
     protected void renderAttachedBlock(GuiGraphics graphics) {
-        GuiGameElement.of(CNBlocks.ENRICHED_SOUL_SOIL)
+        GuiGameElement.of(CNBlocks.ENRICHING_CAMPFIRE.getDefaultState())
                 .scale(SCALE)
                 .atLocal(0, 0, 2)
                 .lighting(AnimatedKinetics.DEFAULT_LIGHTING)
                 .render(graphics);
+    }
+
+    @Override
+    public Component getTitle() {
+        return Component.translatable("createnuclear.enriched.fan.recipe");
     }
 }
