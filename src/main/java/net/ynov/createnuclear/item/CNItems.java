5d21c319859bdfdc8204a6d4cc1b7d2c22c01cda
MathisGredtYnov <113118860+MathisGredtYnov@users.noreply.github.com>
2024-03-03 10:58:43 +0100

    debug texture cloth et craft armure

--- src/main/java/net/ynov/createnuclear/item/CNItems.java
+++ src/main/java/net/ynov/createnuclear/item/CNItems.java
@@ -36,7 +36,7 @@ public class CNItems {
     public static final Item ANTI_RADIATION_CHESTPLATE = registerItem("anti_radiation_chestplate", new ArmorItem(CNArmorMaterials.ANTI_RADIATION_SUIT, ArmorItem.Type.CHESTPLATE, new FabricItemSettings()));
     public static final Item ANTI_RADIATION_LEGGINGS = registerItem("anti_radiation_leggings", new ArmorItem(CNArmorMaterials.ANTI_RADIATION_SUIT, ArmorItem.Type.LEGGINGS, new FabricItemSettings()));
     public static final Item ANTI_RADIATION_BOOTS = registerItem("anti_radiation_boots", new ArmorItem(CNArmorMaterials.ANTI_RADIATION_SUIT, ArmorItem.Type.BOOTS, new FabricItemSettings()));
-    public static final Item CLOTH = registerItem("cloth", new Item(new FabricItemSettings()));
+    public static final Item WHITE_CLOTH = registerItem("white_cloth", new Item(new FabricItemSettings()));
 
     private static void AddItemToIngredientItemGroup(FabricItemGroupEntries entries) {
         entries.accept(URANIUM_POWDER, CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
@@ -58,7 +58,7 @@ public class CNItems {
         entries.accept(ANTI_RADIATION_CHESTPLATE,CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
         entries.accept(ANTI_RADIATION_LEGGINGS,CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
         entries.accept(ANTI_RADIATION_BOOTS,CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
-        entries.accept(CLOTH,CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
+        entries.accept(WHITE_CLOTH,CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS);
     }
 
     private static Item registerItem(String name, Item item) {
