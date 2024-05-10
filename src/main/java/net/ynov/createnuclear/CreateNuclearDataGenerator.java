e0cd629f5a52925df0e8d3e1a5de5a3d6159e304
Giovanni <giovanni.ricotta@ynov.com>
2024-05-10 21:10:40 +0200

    update build.gradle

--- src/main/java/net/ynov/createnuclear/CreateNuclearDataGenerator.java
+++ src/main/java/net/ynov/createnuclear/CreateNuclearDataGenerator.java
@@ -1,26 +1,27 @@
 package net.ynov.createnuclear;
 
+import io.github.fabricators_of_create.porting_lib.data.ExistingFileHelper;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 import net.minecraft.core.RegistrySetBuilder;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.ResourceKey;
 import net.ynov.createnuclear.datagen.CNWorldGenerator;
 import net.ynov.createnuclear.world.CNConfiguredFeatures;
 import net.ynov.createnuclear.world.CNPlacedFeatures;
-import org.apache.http.config.RegistryBuilder;
 
 public class CreateNuclearDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		ExistingFileHelper helper = ExistingFileHelper.withResourcesFromArg();
 		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-
-		pack.addProvider(CNWorldGenerator::new);
+		//CreateNuclear.REGISTRATE.setupDatagen(pack, helper);
+		CreateNuclear.LOGGER.warn(System.getProperty("porting_lib.datagen.existing_resources") + " " + System.getProperty("porting_lib.datagen") + " " + System.getProperty("porting_lib"));
+		//pack.addProvider(CNWorldGenerator::new);
 	}
 
 	@Override
 	public void buildRegistry(RegistrySetBuilder registryBuilder) {
-		registryBuilder.add(Registries.CONFIGURED_FEATURE, CNConfiguredFeatures::boostrap);
-		registryBuilder.add(Registries.PLACED_FEATURE, CNPlacedFeatures::boostrap);
+		//registryBuilder.add(Registries.CONFIGURED_FEATURE, CNConfiguredFeatures::boostrap);
+		//registryBuilder.add(Registries.PLACED_FEATURE, CNPlacedFeatures::boostrap);
 	}
 }
