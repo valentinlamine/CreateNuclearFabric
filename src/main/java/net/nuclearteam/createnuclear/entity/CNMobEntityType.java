05ca79d25d38cc527f194f1d87e98a28934283c1
Giovanni <giovanni.ricotta@ynov.com>
2024-10-19 12:52:23 +0200

    fixDimensionsCustomMob: Fixed

--- src/main/java/net/nuclearteam/createnuclear/entity/CNMobEntityType.java
+++ src/main/java/net/nuclearteam/createnuclear/entity/CNMobEntityType.java
@@ -5,6 +5,7 @@ import com.tterrag.registrate.util.entry.EntityEntry;
 import net.minecraft.core.Registry;
 import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityDimensions;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.nuclearteam.createnuclear.CreateNuclear;
@@ -15,13 +16,13 @@ import net.nuclearteam.createnuclear.entity.irradiatedwolf.IrradiatedWolf;
 public class CNMobEntityType {
 
     public static final EntityType<IrradiatedChicken> IRRADIATED_CHICKEN =
-            CNMobEntityType.register("irradiated_chicken", EntityType.Builder.of(IrradiatedChicken::new, MobCategory.CREATURE));
+            CNMobEntityType.register("irradiated_chicken", EntityType.Builder.of(IrradiatedChicken::new, MobCategory.CREATURE).sized(0.4f, 0.7f));
 
     public static final EntityType<IrradiatedWolf> IRRADIATED_WOLF =
-            CNMobEntityType.register("irradiated_wolf", EntityType.Builder.of(IrradiatedWolf::new, MobCategory.CREATURE));
+            CNMobEntityType.register("irradiated_wolf", EntityType.Builder.of(IrradiatedWolf::new, MobCategory.CREATURE).sized(0.6f, 0.85f));
 
     public static final EntityType<IrradiatedCat> IRRADIATED_CAT =
-            CNMobEntityType.register("irradiated_cat", EntityType.Builder.of(IrradiatedCat::new, MobCategory.CREATURE));
+            CNMobEntityType.register("irradiated_cat", EntityType.Builder.of(IrradiatedCat::new, MobCategory.CREATURE).sized(0.6f, 0.7f));
 
     private static <T extends Entity> EntityType<T> register(String key, EntityType.Builder<T> builder) {
         return Registry.register(BuiltInRegistries.ENTITY_TYPE, key, builder.build(key));
