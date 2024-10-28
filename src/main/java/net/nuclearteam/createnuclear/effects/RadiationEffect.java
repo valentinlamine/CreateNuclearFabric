fc8ffd8a30635e6b9ae4d23d4b98a17875b7a8f0
Giovanni <giovanni.ricotta@ynov.com>
2024-10-28 09:20:35 +0100

    addResisEffecForTagEmunEffect: Updated

--- src/main/java/net/nuclearteam/createnuclear/effects/RadiationEffect.java
+++ src/main/java/net/nuclearteam/createnuclear/effects/RadiationEffect.java
@@ -21,17 +21,14 @@ public class RadiationEffect extends MobEffect {
     public void applyEffectTick(LivingEntity livingEntity, int amplifier) {
         //Si le joueur porte l'armure anti_radiation_suit alors il ne prend pas de dégâts
         livingEntity.getArmorSlots().forEach(e -> {
-            if ((livingEntity.hasEffect(CNEffects.RADIATION.get()) && AntiRadiationArmorItem.Armor.isArmored2(e)) || livingEntity.getType().is(CNTag.EntityTypeTags.IRRADIATED_IMMUNE.tag)) {
-                CreateNuclear.LOGGER.warn("entity: {}, AntiRadiationArmorItem: {}", livingEntity, AntiRadiationArmorItem.Armor.isArmored2(e));
-                livingEntity.removeEffect(this);
-                //livingEntity.hurt(livingEntity.damageSources().magic(), 0.0F);
-
+            if (livingEntity.hasEffect(CNEffects.RADIATION.get()) && AntiRadiationArmorItem.Armor.isArmored2(e)) {
+                livingEntity.hurt(livingEntity.damageSources().magic(), 0.0F);
             }
-            /*else if (livingEntity.getType().is(CNTag.EntityTypeTags.IRRADIATED_IMMUNE.tag)) {
+            else if (livingEntity.getType().is(CNTag.EntityTypeTags.IRRADIATED_IMMUNE.tag)) {
                 livingEntity.removeEffect(this);
-            }*/
+            }
             else {
-                livingEntity.hurt(livingEntity.damageSources().magic(), 1.0F);
+                livingEntity.hurt(livingEntity.damageSources().magic(), 1 << amplifier);
             }
         });
     }
