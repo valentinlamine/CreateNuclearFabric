056c846f2a9497b3f2d732dcf772ff527afc7bbb
Giovanni <giovanni.ricotta@ynov.com>
2024-11-08 18:39:11 +0100

    ChangeMethodeCreateIrradiatedMob: Updated

--- src/main/java/net/nuclearteam/createnuclear/tools/CustomSpawnEgg.java
+++ src/main/java/net/nuclearteam/createnuclear/tools/CustomSpawnEgg.java
@@ -3,11 +3,9 @@ package net.nuclearteam.createnuclear.tools;
 import com.tterrag.registrate.util.entry.EntityEntry;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
-import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.MobSpawnType;
 import net.minecraft.world.item.ItemStack;
@@ -19,17 +17,15 @@ import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.SpawnerBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.gameevent.GameEvent;
-import net.nuclearteam.createnuclear.CreateNuclear;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 
 public class CustomSpawnEgg extends SpawnEggItem {
     private final EntityEntry<? extends Mob> test;
-    private final EntityType<?> defaultType;
 
     public CustomSpawnEgg(Properties properties, int backgroundColor, int highlightColor, EntityEntry<? extends Mob> test) {
         super(test.get(), backgroundColor, highlightColor, properties);
         this.test = test;
-        this.defaultType = test.get();
     }
 
     @Override
@@ -55,20 +51,11 @@ public class CustomSpawnEgg extends SpawnEggItem {
 
         BlockPos blockPos2 = blockState.getCollisionShape(level, blockPos).isEmpty() ? blockPos : blockPos.relative(direction);
 
-        if (EntityEntry.cast(this.test).lazy().get().spawn((ServerLevel) context.getLevel(), context.getItemInHand(), context.getPlayer(), blockPos2, MobSpawnType.SPAWN_EGG, true, true) != null) {
+        if (EntityEntry.cast(this.test).lazy().get().spawn((ServerLevel)level, itemStack, context.getPlayer(), blockPos2, MobSpawnType.SPAWN_EGG, true, !Objects.equals(blockPos, blockPos2) && direction == Direction.UP) != null) {
             itemStack.shrink(1);
             level.gameEvent((Entity)context.getPlayer(), GameEvent.ENTITY_PLACE, blockPos);
             return InteractionResult.SUCCESS;
         }
         return super.useOn(context);
-    }
-
-    public EntityType<?> getType(@Nullable CompoundTag nbt) {
-        CompoundTag compoundTag;
-        if (nbt != null && nbt.contains("EntityTag", 10) && (compoundTag = nbt.getCompound("EntityTag")).contains("id", 8)) {
-            CreateNuclear.LOGGER.warn("getType: {}, {}", compoundTag.getString("id"), EntityType.byString(compoundTag.getString("id")));
-            return EntityType.byString(compoundTag.getString("id")).orElse(this.defaultType);
-        }
-        return this.defaultType;
     }
 }
