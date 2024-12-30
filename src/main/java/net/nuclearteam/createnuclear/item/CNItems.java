6e17dc328e6a0c68c4ad30f1a6b95914d3c2cc11
Giovanni <giovanni.ricotta@ynov.com>
2024-12-30 19:39:51 +0100

    Fix-PreV1: Removed eated item

--- src/main/java/net/nuclearteam/createnuclear/item/CNItems.java
+++ src/main/java/net/nuclearteam/createnuclear/item/CNItems.java
@@ -45,18 +45,6 @@ public class CNItems {
 
         YELLOWCAKE = CreateNuclear.REGISTRATE
                 .item("yellowcake", Item::new)
-                .properties(p -> p.food(new FoodProperties.Builder()
-                    .nutrition(20)
-                    .saturationMod(0.3F)
-                    .alwaysEat()
-                    .effect(new MobEffectInstance(CNEffects.RADIATION.get(), 6000, 25), 1.0F)
-                    .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 6000, 5), 1.0F)
-                    .effect(new MobEffectInstance(MobEffects.HUNGER, 6000, 1000), 1.0F)
-                    .effect(new MobEffectInstance(MobEffects.CONFUSION, 6000, 5), 1.0F)
-                    .effect(new MobEffectInstance(MobEffects.WITHER, 6000, 8), 1.0F)
-                    .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 6000, 5), 1.0F)
-                    .build())
-                )
                 .register(),
 
         ENRICHED_YELLOWCAKE = CreateNuclear.REGISTRATE
