b47de4f51935d5784c4a4635ee01441eb519b800
matteoVcs <matteo.vocanson@ynov.com>
2025-01-09 12:43:59 +0100

    cannot open input with blocks in hand

--- src/main/java/net/nuclearteam/createnuclear/multiblock/energy/ReactorOutputEntity.java
+++ src/main/java/net/nuclearteam/createnuclear/multiblock/energy/ReactorOutputEntity.java
@@ -73,7 +73,6 @@ public class ReactorOutputEntity extends GeneratingKineticBlockEntity {
 			if (controllerEntity != null) {
 				if (!controllerEntity.getAssembled() && getSpeed() != 0) {
 					setSpeed(0);
-					CreateNuclear.LOGGER.warn("ReactorOutputEntity : Controller not found");
 				}
 			}
 		}
@@ -125,7 +124,6 @@ public class ReactorOutputEntity extends GeneratingKineticBlockEntity {
 
 	public void setSpeed(int speed) {
 		this.speed = speed;
-		CreateNuclear.LOGGER.warn(speed + " ReactorOutputEntity " + this.speed);
 	}
 
 
