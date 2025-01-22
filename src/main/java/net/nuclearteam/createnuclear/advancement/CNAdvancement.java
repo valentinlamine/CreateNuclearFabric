10957aeeda7a2d86c0f956758a9945eb98febd3c
valentinlamine <github@valentinlamine.fr>
2025-01-22 19:21:25 +0100

    Changing advancements name & description + fix of hidden advancements

--- src/main/java/net/nuclearteam/createnuclear/advancement/CNAdvancement.java
+++ src/main/java/net/nuclearteam/createnuclear/advancement/CNAdvancement.java
@@ -40,44 +40,44 @@ public class CNAdvancement implements DataProvider {
     public static final CreateNuclearAdvancement START = null,
 
     ROOT = create("root", b -> b.icon(CNItems.URANIUM_POWDER)
-            .title("Welcome to Create Nuclear")
-			.description("New era of energy")
+            .title("Starting The Nuclear Journey")
+			.description("Unlock the basics of nuclear energy and get your first uranium powder")
 			.awardedForFree()
 			.special(SILENT)),
 
     RAW_URANIUM = create("raw_uranium", b -> b.icon(CNItems.RAW_URANIUM)
-            .title("Raw Uranium")
-			.description("Obtain some raw uranium by mining uranium ore")
+            .title("The Raw Power")
+			.description("Mine uranium ore to obtain raw uranium for further processing")
 			.after(ROOT)
 			.whenIconCollected()),
 
     URANIUM_POWDER = create("uranium_powder", b -> b.icon(CNItems.URANIUM_POWDER)
-            .title("Uranium Powder")
-            .description("Obtain some uranium powder by crushing raw uranium")
+            .title("Powdered Uranium")
+            .description("Crush raw uranium into powder to prepare for further refining")
             .after(RAW_URANIUM)
             .whenIconCollected()),
 
     URANIUM_LIQUID = create("uranium_liquid", b -> b.icon(CNFluids.URANIUM.getBucket().get())
-            .title("Uranium Liquid")
+            .title("Turning Solid To Liquid")
             .description("Obtain some uranium liquid by mixing uranium powder")
             .after(URANIUM_POWDER)
             .whenIconCollected()),
 
     YELLOWCAKE = create("yellowcake", b -> b.icon(CNItems.YELLOWCAKE)
-            .title("Yellowcake")
-            .description("Obtain some yellowcake by compacting uranium liquid")
+            .title("The Yellowcake Process")
+            .description("Compact uranium liquid to create yellowcake")
             .after(URANIUM_LIQUID)
             .whenIconCollected()),
 
     ENRICHED_YELLOWCAKE = create("enriched_yellowcake", b -> b.icon(CNItems.ENRICHED_YELLOWCAKE)
-            .title("Enriched Yellow Cake")
-            .description("Obtain some enriched yellow cake by enriching yellowcake with a fan")
+            .title("Enhancing Yellowcake")
+            .description("Use a fan to enrich yellowcake and make it more powerful")
             .after(YELLOWCAKE)
             .whenIconCollected()),
 
     URANIUM_ROD = create("uranium_rod", b -> b.icon(CNItems.URANIUM_ROD)
-            .title("Uranium Rod")
-            .description("Craft your first uranium rod using enriched yellowcake in a mechanical crafter")
+            .title("The Power Of The Atom")
+            .description("Create your first uranium rod using enriched yellowcake in a mechanical crafter")
             .after(ENRICHED_YELLOWCAKE)
             .whenIconCollected()),
 
@@ -86,25 +86,25 @@ public class CNAdvancement implements DataProvider {
 
     COAL_DUST = create("coal_dust", b -> b.icon(CNItems.COAL_DUST)
             .title("Coal Dust")
-            .description("Obtain some coal dust by crushing coal or charcoal")
+            .description("Crush coal or charcoal to obtain coal dust, a key crafting material")
             .after(ROOT)
             .whenIconCollected()),
 
     STEEL_INGOT = create("steel_ingot", b -> b.icon(CNItems.STEEL_INGOT)
             .title("Steel Ingot")
-            .description("Obtain some steel ingot by mixing iron ingot and coal dust")
+            .description("Combine coal dust and iron ingots to create steel ingots")
             .after(COAL_DUST)
             .whenIconCollected()),
 
     GRAPHENE = create("graphene", b -> b.icon(CNItems.GRAPHENE)
             .title("Graphene")
-            .description("Obtain some graphene by pressing coal dust")
+            .description("Press coal dust to create graphene")
             .after(COAL_DUST)
             .whenIconCollected()),
 
     GRAPITE_ROD = create("graphite_rod", b -> b.icon(CNItems.GRAPHITE_ROD)
-            .title("Graphite Rod")
-            .description("Craft your first graphite rod using graphene and steel ingot in a mechanical crafter")
+            .title("Don't Forget Those Ones")
+            .description("Combine graphene and steel ingots in a mechanical crafter to make graphite rods")
             .after(GRAPHENE)
             .whenIconCollected()),
 
@@ -133,87 +133,79 @@ public class CNAdvancement implements DataProvider {
             .whenIconCollected()),
 
     ANTI_RADIATION_ARMOR = create("anti_radiation_armor", b -> b.icon(CNItems.ANTI_RADIATION_HELMETS.get(DyeColor.WHITE))
-            .title("Anti radiation armor")
-            .description("Craft your first anti radiation armor piece to protect yourself from radiation")
+            .title("Anti radiation Armor")
+            .description("Craft your first anti-radiation armor piece to protect yourself from radiation")
             .after(LEAD_INGOT)
             .whenItemCollected(CNTag.ItemTags.ALL_ANTI_RADIATION_ARMORS.tag)),
 
     FULL_ANTI_RADIATION_ARMOR = create("full_anti_radiation_armor", b -> b.icon(CNItems.ANTI_RADIATION_CHESTPLATES.get(DyeColor.WHITE))
-            .title("Anti radiation armor")
-            .description("Wear a full anti radiation armor to protect yourself from radiation")
+            .title("Fully Protected")
+            .description("Wear a full set of anti-radiation armor to fully protect yourself from radiation")
             .externalTrigger(InventoryChangeTrigger.TriggerInstance.hasItems(new ItemLike[]{CNItems.ANTI_RADIATION_LEGGINGS.get(DyeColor.WHITE), CNItems.ANTI_RADIATION_CHESTPLATES.get(DyeColor.WHITE), CNItems.ANTI_RADIATION_HELMETS.get(DyeColor.WHITE), CNItems.ANTI_RADIATION_BOOTS}))
             .after(ANTI_RADIATION_ARMOR)),
 
     DYE_ANTI_RADIATION_ARMOR = create("dye_anti_radiation_armor", b -> b.icon(CNItems.ANTI_RADIATION_HELMETS.get(DyeColor.RED))
-            .title("Anti radiation armor")
+            .title("Pimp My Armor")
             .description("Dye your anti radiation armor to any color")
             .whenItemCollected(CNTag.ItemTags.ANTI_RADIATION_HELMET_DYE.tag)
             .whenItemCollected(CNTag.ItemTags.ANTI_RADIATION_CHESTPLATE_DYE.tag)
             .whenItemCollected(CNTag.ItemTags.ANTI_RADIATION_LEGGINGS_DYE.tag)
-            .whenItemCollected(CNTag.ItemTags.ANTI_RADIATION_BOOTS_DYE.tag)
             .after(ANTI_RADIATION_ARMOR)),
 
 
     AUTOMATIC_URANIUM = create("automatic_uranium", b -> b.icon(CNItems.URANIUM_POWDER)
-            .title("Uranium Powder")
+            .title("Automating Uranium")
             .description("Obtain some uranium powder using the automatic uranium processing")
             .after(ROOT)
             .special(SECRET)),
 
     REACTOR_CASING = create("reactor_casing", b -> b.icon(CNBlocks.REACTOR_CASING)
-            .title("Reactor Casing")
-            .description("Craft a reactor casing to build your nuclear reactor")
+            .title("The Power Of The Reactor")
+            .description("Craft a reactor casing to begin building your nuclear reactor")
             .after(ROOT)
-
             .whenIconCollected()),
 
     REACTOR_CONTROLLER = create("reactor_controller", b -> b.icon(CNBlocks.REACTOR_CONTROLLER)
-            .title("Reactor Controller")
-            .description("Craft a reactor controller to control your nuclear reactor")
+            .title("Controller Of The Core")
+            .description("Craft a reactor controller to manage and regulate your reactor")
             .after(REACTOR_CASING)
-            .special(SECRET)
             .whenIconCollected()),
 
 
     REACTOR_BLUEPRINT = create("reactor_blueprint", b -> b.icon(CNItems.CONFIGURED_REACTOR_ITEM)
-            .title("Reactor Blueprint")
-            .description("Craft a reactor blueprint to create your pattern of rods !")
+            .title("Blueprint For Power")
+            .description("Craft a reactor blueprint to design the layout of rods in your reactor")
             .after(REACTOR_CONTROLLER)
             .whenIconCollected()),
 
     REACTOR_COOLING_FRAME = create("reactor_cooling_frame", b -> b.icon(CNBlocks.REACTOR_COOLING_FRAME)
-            .title("Reactor Colling Frame")
-            .description("Craft a reactor colling frame to cool your nuclear reactor")
+            .title("Cooling The Reactor")
+            .description("Craft a reactor cooling frame to cool your reactor")
             .after(REACTOR_CASING)
-            .special(SECRET)
             .whenIconCollected()),
 
     REACTOR_MAIN_FRAME = create("reactor_main_frame", b -> b.icon(CNBlocks.REACTOR_MAIN_FRAME)
             .title("Reactor Main Frame")
             .description("Craft a reactor main frame to build your nuclear reactor")
             .after(REACTOR_CASING)
-            .special(SECRET)
             .whenIconCollected()),
 
     REACTOR_INPUT = create("reactor_input", b -> b.icon(CNBlocks.REACTOR_INPUT)
-            .title("Reactor Input")
-            .description("Craft a reactor input to input uranium and graphite rod to your nuclear reactor")
+            .title("Fueling The Reactor")
+            .description("Craft a reactor input to feed uranium and graphite rods into your reactor")
             .after(REACTOR_CASING)
-            .special(SECRET)
             .whenIconCollected()),
 
     REACTOR_OUTPUT = create("reactor_output", b -> b.icon(CNBlocks.REACTOR_OUTPUT)
-            .title("Reactor Output")
-            .description("Craft a reactor output to output SU to your nuclear reactor")
+            .title("Power Output")
+            .description("Craft a reactor output to transfer the energy produced by your reactor")
             .after(REACTOR_CASING)
-            .special(SECRET)
             .whenIconCollected()),
 
     REACTOR_CORE = create("reactor_core", b -> b.icon(CNBlocks.REACTOR_CORE)
-            .title("Reactor Core")
-            .description("Craft a reactor core to build your nuclear reactor")
+            .title("Core of Power")
+            .description("Craft the reactor core to harness the full energy of your nuclear reactor")
             .after(REACTOR_CASING)
-            .special(SECRET)
             .whenIconCollected())
 
 
