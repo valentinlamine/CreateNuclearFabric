6f92fd5bd597332d98a8f68c7f36174f8e9f5f7a
Giovanni <giovanni.ricotta@ynov.com>
2025-02-06 12:13:35 +0100

    convertMobForFan: Updated

--- src/main/java/net/nuclearteam/createnuclear/fan/CNFanProcessingTypes.java
+++ src/main/java/net/nuclearteam/createnuclear/fan/CNFanProcessingTypes.java
@@ -6,13 +6,20 @@ import com.simibubi.create.content.kinetics.fan.processing.FanProcessingTypeRegi
 import com.simibubi.create.foundation.recipe.RecipeApplier;
 import com.simibubi.create.foundation.utility.Color;
 import com.simibubi.create.foundation.utility.VecHelper;
+import io.github.fabricators_of_create.porting_lib.util.NBTSerializer;
 import it.unimi.dsi.fastutil.objects.Object2ReferenceOpenHashMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.sounds.SoundEvents;
+import net.minecraft.sounds.SoundSource;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.horse.Horse;
+import net.minecraft.world.entity.animal.horse.SkeletonHorse;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -117,9 +124,54 @@ public class CNFanProcessingTypes extends AllFanProcessingTypes {
 
         @Override
         public void affectEntity(Entity entity, Level level) {
-            if (entity instanceof LivingEntity livingEntity) {
-                livingEntity.addEffect(new MobEffectInstance(CNEffects.RADIATION.get(), 10, 0, true, true));
+            if (level.isClientSide) {
+                if (entity instanceof Horse) {
+                    Vec3 p = entity.getPosition(0);
+                    Vec3 v = p.add(0, 0.5f, 0)
+                            .add(VecHelper.offsetRandomly(Vec3.ZERO, level.random, 1)
+                                    .multiply(1, 0.2f, 1)
+                                    .normalize()
+                                    .scale(1f));
+                    level.addParticle(ParticleTypes.SOUL_FIRE_FLAME, v.x, v.y, v.z, 0, 0.1f, 0);
+                    if (level.random.nextInt(3) == 0)
+                        level.addParticle(ParticleTypes.LARGE_SMOKE, p.x, p.y + .5f, p.z,
+                                (level.random.nextFloat() - .5f) * .5f, 0.1f, (level.random.nextFloat() - .5f) * .5f);
+                }
+                return;
             }
+
+            if (entity instanceof LivingEntity livingEntity) {
+                //livingEntity.addEffect(new MobEffectInstance(CNEffects.RADIATION.get(), 10, 0, true, true));
+            }
+
+            /*if (entity instanceof Horse horse) {
+                int progress = horse.getCustomData()
+                        .method_10550("CreateHaunting");
+                if (progress < 100) {
+                    if (progress % 10 == 0) {
+                        level.playSound(null, entity.blockPosition(), SoundEvents.SOUL_ESCAPE, SoundSource.NEUTRAL,
+                                1f, 1.5f * progress / 100f);
+                    }
+                    horse.getCustomData()
+                            .method_10569("CreateHaunting", progress + 1);
+                    return;
+                }
+
+                level.playSound(null, entity.blockPosition(), SoundEvents.GENERIC_EXTINGUISH_FIRE,
+                        SoundSource.NEUTRAL, 1.25f, 0.65f);
+
+                SkeletonHorse skeletonHorse = EntityType.SKELETON_HORSE.create(level);
+                CompoundTag serializeNBT = horse.saveWithoutId(new CompoundTag());
+                serializeNBT.remove("UUID");
+                if (!horse.getArmor()
+                        .isEmpty())
+                    horse.spawnAtLocation(horse.getArmor());
+
+                NBTSerializer.deserializeNBT(skeletonHorse, serializeNBT);
+                skeletonHorse.setPos(horse.getPosition(0));
+                level.addFreshEntity(skeletonHorse);
+                horse.discard();
+            }*/
         }
     }
 }
