36f8a5138f6c1dbd411bc51108ba743d82f4e884
Giovanni <giovanni.ricotta@ynov.com>
2025-02-26 11:57:44 +0100

    convertMobForFan: Updated converte mob

--- src/main/java/net/nuclearteam/createnuclear/fan/CNFanProcessingTypes.java
+++ src/main/java/net/nuclearteam/createnuclear/fan/CNFanProcessingTypes.java
@@ -18,6 +18,9 @@ import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Cat;
+import net.minecraft.world.entity.animal.Chicken;
+import net.minecraft.world.entity.animal.Wolf;
 import net.minecraft.world.entity.animal.horse.Horse;
 import net.minecraft.world.entity.animal.horse.SkeletonHorse;
 import net.minecraft.world.item.ItemStack;
@@ -27,6 +30,10 @@ import net.minecraft.world.phys.Vec3;
 import net.nuclearteam.createnuclear.CreateNuclear;
 import net.nuclearteam.createnuclear.block.CNBlocks;
 import net.nuclearteam.createnuclear.effects.CNEffects;
+import net.nuclearteam.createnuclear.entity.CNMobEntityType;
+import net.nuclearteam.createnuclear.entity.irradiatedcat.IrradiatedCat;
+import net.nuclearteam.createnuclear.entity.irradiatedchicken.IrradiatedChicken;
+import net.nuclearteam.createnuclear.entity.irradiatedwolf.IrradiatedWolf;
 import net.nuclearteam.createnuclear.fan.EnrichedRecipe.EnrichedWrapper;
 import net.nuclearteam.createnuclear.tools.EnrichingCampfireBlock;
 
@@ -125,7 +132,7 @@ public class CNFanProcessingTypes extends AllFanProcessingTypes {
         @Override
         public void affectEntity(Entity entity, Level level) {
             if (level.isClientSide) {
-                if (entity instanceof Horse) {
+                if (entity instanceof Chicken || entity instanceof Wolf || entity instanceof Cat) {
                     Vec3 p = entity.getPosition(0);
                     Vec3 v = p.add(0, 0.5f, 0)
                             .add(VecHelper.offsetRandomly(Vec3.ZERO, level.random, 1)
@@ -134,7 +141,7 @@ public class CNFanProcessingTypes extends AllFanProcessingTypes {
                                     .scale(1f));
                     level.addParticle(ParticleTypes.SOUL_FIRE_FLAME, v.x, v.y, v.z, 0, 0.1f, 0);
                     if (level.random.nextInt(3) == 0)
-                        level.addParticle(ParticleTypes.LARGE_SMOKE, p.x, p.y + .5f, p.z,
+                        level.addParticle(ParticleTypes.SOUL, p.x, p.y + .5f, p.z,
                                 (level.random.nextFloat() - .5f) * .5f, 0.1f, (level.random.nextFloat() - .5f) * .5f);
                 }
                 return;
@@ -144,34 +151,77 @@ public class CNFanProcessingTypes extends AllFanProcessingTypes {
                 //livingEntity.addEffect(new MobEffectInstance(CNEffects.RADIATION.get(), 10, 0, true, true));
             }
 
-            /*if (entity instanceof Horse horse) {
-                int progress = horse.getCustomData()
-                        .method_10550("CreateHaunting");
+            if (entity instanceof Chicken chicken) {
+                int progress = chicken.getCustomData().getInt("CreateNuclearRadiationChicken");
                 if (progress < 100) {
                     if (progress % 10 == 0) {
                         level.playSound(null, entity.blockPosition(), SoundEvents.SOUL_ESCAPE, SoundSource.NEUTRAL,
                                 1f, 1.5f * progress / 100f);
                     }
-                    horse.getCustomData()
-                            .method_10569("CreateHaunting", progress + 1);
+                    chicken.getCustomData().putInt("CreateNuclearRadiationChicken", progress + 1);
                     return;
                 }
 
                 level.playSound(null, entity.blockPosition(), SoundEvents.GENERIC_EXTINGUISH_FIRE,
                         SoundSource.NEUTRAL, 1.25f, 0.65f);
 
-                SkeletonHorse skeletonHorse = EntityType.SKELETON_HORSE.create(level);
-                CompoundTag serializeNBT = horse.saveWithoutId(new CompoundTag());
+                IrradiatedChicken IrradiatedChicken = CNMobEntityType.IRRADIATED_CHICKEN.create(level);
+                CompoundTag serializeNBT = chicken.saveWithoutId(new CompoundTag());
                 serializeNBT.remove("UUID");
-                if (!horse.getArmor()
-                        .isEmpty())
-                    horse.spawnAtLocation(horse.getArmor());
 
-                NBTSerializer.deserializeNBT(skeletonHorse, serializeNBT);
-                skeletonHorse.setPos(horse.getPosition(0));
-                level.addFreshEntity(skeletonHorse);
-                horse.discard();
-            }*/
+                NBTSerializer.deserializeNBT(IrradiatedChicken, serializeNBT);
+                IrradiatedChicken.setPos(chicken.getPosition(0));
+                level.addFreshEntity(IrradiatedChicken);
+                chicken.discard();
+            }
+
+            if (entity instanceof Wolf wolf) {
+                int progress = wolf.getCustomData().getInt("CreateNuclearRadiationWolf");
+                if (progress < 100) {
+                    if (progress % 10 == 0) {
+                        level.playSound(null, entity.blockPosition(), SoundEvents.SOUL_ESCAPE, SoundSource.NEUTRAL,
+                                1f, 1.5f * progress / 100f);
+                    }
+                    wolf.getCustomData().putInt("CreateNuclearRadiationWolf", progress + 1);
+                    return;
+                }
+
+                level.playSound(null, entity.blockPosition(), SoundEvents.GENERIC_EXTINGUISH_FIRE,
+                        SoundSource.NEUTRAL, 1.25f, 0.65f);
+
+                IrradiatedWolf IrradiatedWolf = CNMobEntityType.IRRADIATED_WOLF.create(level);
+                CompoundTag serializeNBT = wolf.saveWithoutId(new CompoundTag());
+                serializeNBT.remove("UUID");
+
+                NBTSerializer.deserializeNBT(IrradiatedWolf, serializeNBT);
+                IrradiatedWolf.setPos(wolf.getPosition(0));
+                level.addFreshEntity(IrradiatedWolf);
+                wolf.discard();
+            }
+
+            if (entity instanceof Cat cat) {
+                int progress = cat.getCustomData().getInt("CreateNuclearRadiationCat");
+                if (progress < 100) {
+                    if (progress % 10 == 0) {
+                        level.playSound(null, entity.blockPosition(), SoundEvents.SOUL_ESCAPE, SoundSource.NEUTRAL,
+                                1f, 1.5f * progress / 100f);
+                    }
+                    cat.getCustomData().putInt("CreateNuclearRadiationCat", progress + 1);
+                    return;
+                }
+
+                level.playSound(null, entity.blockPosition(), SoundEvents.GENERIC_EXTINGUISH_FIRE,
+                        SoundSource.NEUTRAL, 1.25f, 0.65f);
+
+                IrradiatedCat IrradiatedCat = CNMobEntityType.IRRADIATED_CAT.create(level);
+                CompoundTag serializeNBT = cat.saveWithoutId(new CompoundTag());
+                serializeNBT.remove("UUID");
+
+                NBTSerializer.deserializeNBT(IrradiatedCat, serializeNBT);
+                IrradiatedCat.setPos(cat.getPosition(0));
+                level.addFreshEntity(IrradiatedCat);
+                cat.discard();
+            }
         }
     }
 }
