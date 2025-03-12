c99debd80dc18d69b14457d77c1594a60d93ae4b
Giovanni <giovanni.ricotta@ynov.com>
2025-03-12 11:49:49 +0100

    eatable-yellowcake: Updated

--- src/main/java/net/nuclearteam/createnuclear/effects/RadiationEffect.java
+++ src/main/java/net/nuclearteam/createnuclear/effects/RadiationEffect.java
@@ -1,20 +1,21 @@
 package net.nuclearteam.createnuclear.effects;
 
+import io.github.fabricators_of_create.porting_lib.entity.client.MobEffectRenderer;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.attributes.Attribute;
-import net.minecraft.world.entity.ai.attributes.AttributeModifier;
-import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.ai.attributes.*;
 import net.nuclearteam.createnuclear.CreateNuclear;
 import net.nuclearteam.createnuclear.item.armor.AntiRadiationArmorItem;
 import net.nuclearteam.createnuclear.tags.CNTag;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
 
 public class RadiationEffect extends MobEffect {
     public RadiationEffect() {
         super(MobEffectCategory.HARMFUL, 15453236);
-
-
     }
 
     @Override
@@ -24,16 +25,18 @@ public class RadiationEffect extends MobEffect {
 
     @Override
     public void applyEffectTick(LivingEntity livingEntity, int amplifier) {
-        //If the player has the radiation effect and is wearing the anti-radiation armor, they will not take damage
+        //Si le joueur porte l'armure anti_radiation_suit alors il ne prend pas de dégâts
         livingEntity.getArmorSlots().forEach(e -> {
             if (livingEntity.hasEffect(CNEffects.RADIATION.get()) && AntiRadiationArmorItem.Armor.isArmored2(e)) {
                 livingEntity.hurt(livingEntity.damageSources().magic(), 0.0F);
+                this.removeAttributeModifiers(livingEntity, livingEntity.getAttributes(), 15);
             }
             else if (livingEntity.getType().is(CNTag.EntityTypeTags.IRRADIATED_IMMUNE.tag)) {
                 livingEntity.removeEffect(this);
             }
             else {
                 livingEntity.hurt(livingEntity.damageSources().magic(), 1 << amplifier);
+                this.addAttributeModifier(Attributes.MOVEMENT_SPEED, "7107DE5E-7CE8-4030-940E-514C1F160890", 15.0,  AttributeModifier.Operation.MULTIPLY_TOTAL);
             }
         });
     }
