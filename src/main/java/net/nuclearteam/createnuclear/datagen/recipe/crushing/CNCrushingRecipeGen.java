a645bcf76370a39df0542cce826ebabb07d6a444
matteoVcs <matteo.vocanson@ynov.com>
2025-03-12 11:54:23 +0100

    need fix on crushing recipe

--- src/main/java/net/nuclearteam/createnuclear/datagen/recipe/crushing/CNCrushingRecipeGen.java
+++ src/main/java/net/nuclearteam/createnuclear/datagen/recipe/crushing/CNCrushingRecipeGen.java
@@ -1,6 +1,8 @@
 package net.nuclearteam.createnuclear.datagen.recipe.crushing;
 
+import com.simibubi.create.AllItems;
 import com.simibubi.create.AllRecipeTypes;
+import com.simibubi.create.Create;
 import com.simibubi.create.content.processing.recipe.ProcessingRecipe;
 import com.simibubi.create.content.processing.recipe.ProcessingRecipeBuilder;
 import com.simibubi.create.content.processing.recipe.ProcessingRecipeSerializer;
@@ -33,10 +35,20 @@ public class CNCrushingRecipeGen extends ProcessingRecipeGen {
         GRANITE_URANIUM_POWDER = create(() -> Items.GRANITE, b -> b.duration(250)
                 .output(.5f, CNItems.URANIUM_POWDER)
                 .output(1f, Blocks.RED_SAND)
+        ),
+
+        RAW_ZINC = create(() -> AllItems.RAW_ZINC, b -> b.duration(250)
+                .output(1, AllItems.CRUSHED_ZINC, 1)
+                .output(.75f, AllItems.EXP_NUGGET, 1)
+                .output(.25f, CNItems.LEAD_NUGGET,1)
+        ),
+        RAW_COPPER = create(() -> Items.RAW_COPPER, b -> b.duration(250)
+                .output(1, AllItems.CRUSHED_COPPER, 1)
+                .output(.75f, AllItems.EXP_NUGGET, 1)
+                .output(.15f, CNItems.LEAD_NUGGET,1)
         )
 
     ;
-
     GeneratedRecipe
         RAW_URANIUM = create(() -> CNItems.RAW_URANIUM, b -> b.duration(250)
                 .output(1, CNItems.URANIUM_POWDER,9)
